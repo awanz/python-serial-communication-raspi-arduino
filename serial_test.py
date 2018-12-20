3fd30484f8bf92f9539d2e685e6fcb6e7399fb99
Hermawan <jajang@student.telkomuniversity.ac.id>
2018-12-20 23:51:49 +0700

    update tabs

--- serial_test.py
+++ serial_test.py
@@ -5,11 +5,11 @@ ser = serial.Serial('/dev/ttyACM0',9600)
 s = [0,1]
 a = 0;
 with open('hasil.csv', 'w', newline='') as f:
-	filednames = ['column1', 'column2']
-    	thewriter = csv.DictWriter(f, fieldnames=filednames)
-    	thewriter.writeheader()
-	while True:
-		#s[0] = str(int(ser.readline(), 16))
-		serialstring = "hai"
-		print(serialstring)
-		thewriter.writerow({'column1': s[0], 'column2': "tes"})
+    filednames = ['column1', 'column2']
+    thewriter = csv.DictWriter(f, fieldnames=filednames)
+    thewriter.writeheader()
+    while True:
+        #s[0] = str(int(ser.readline(), 16))
+        serialstring = "hai"
+        print(serialstring)
+        thewriter.writerow({'column1': s[0], 'column2': "tes"})
