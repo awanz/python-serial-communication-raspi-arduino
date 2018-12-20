4d3cd430ce52bcf31e1914041cfdb02090125407
Awan <jajang@student.telkomuniversity.ac.id>
2018-12-20 16:48:01 +0000

    Serial Raspberry

---
+++ tugas4-2.py
@@ -0,0 +1,15 @@
+import serial
+import csv
+
+ser = serial.Serial('/dev/ttyACM0',9600)
+s = [0,1]
+a = 0;
+with open('hasil.csv', 'w', newline='') as f:
+	filednames = ['column1', 'column2']
+    	thewriter = csv.DictWriter(f, fieldnames=filednames)
+    	thewriter.writeheader()
+	while True:
+		#s[0] = str(int(ser.readline(), 16))
+		serialstring = "hai"
+		print(serialstring)
+		thewriter.writerow({'column1': s[0], 'column2': "tes"})
